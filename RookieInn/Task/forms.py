3d69b9df3f94bf7b783a98c80ac09f8edd3e5d43
shakboo <910298576@qq.com>
2018-10-15 23:51:05 +0800

    V1.0
    点位管理网站已经完成出版，准备编写学习资料分享网站

--- RookieInn/Task/forms.py
+++
@@ -1,8 +0,0 @@
-#coding=utf-8
-
-from django.contrib.auth.forms import UserCreationForm
-from .models import User
-
-class RegisterForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
-        model = User
--- RookieInn/Task/urls.py
+++
@@ -1,8 +0,0 @@
-#conding=utf-8
-
-from django.conf.urls import url
-from . import views
-
-app_name = "Task"
-urlpatterns = [
-]
